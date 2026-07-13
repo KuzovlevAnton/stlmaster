5801fe500b2b7a49714db1760e3b3d6a1e83d493
KuzovlevAnton <124177177+KuzovlevAnton@users.noreply.github.com>
2026-07-14 00:23:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from read import read_file
 from render import render
 import math
 from get_charset import brightness_sort
+from separate import Separator
 import time
 import os
 
-header, polygons, dimensions = read_file("octahedron0.stl")
+# header, polygons, dimensions = read_file("octahedron0.stl")
 # header, polygons, dimensions = read_file("tetrahedron0.stl")
 # header, polygons, dimensions = read_file("cube.stl")
+# header, polygons, dimensions = read_file("flashlight73.stl")
+header, polygons, dimensions = read_file("_ОСНОВА ШУСТРИК3 ФУТБОЛ 20042026 (1).stl")
 # header, polygons, dimensions = read_file("sphere.stl")
+# header, polygons, dimensions = read_file("separatetest.stl")
+
 
 
 center = Point(
@@ -21,9 +26,12 @@ diagonal = ((dimensions[0]-dimensions[1])**2+(dimensions[2]-dimensions[3])**2+(d
 
 # print(center, diagonal)
 
-camera = Vector(1,1,1).normalize()
-# angle=10
-os.system("clear")
-for angle in range(0, 3600, 1):
-    render(polygons, diagonal, center, 100, 80, camera=Vector(camera.x*math.cos(math.radians(angle))-camera.y*math.sin(math.radians(angle)), camera.x*math.sin(math.radians(angle))+camera.y*math.cos(math.radians(angle)), camera.z), chars=brightness_sort())
-    time.sleep(0.01)
+
+# camera = Vector(1,1,1).normalize()
+# os.system("clear")
+# for angle in range(0, 3600, 1):
+#     render(polygons, diagonal, center, 100, 80, camera=Vector(camera.x*math.cos(math.radians(angle))-camera.y*math.sin(math.radians(angle)), camera.x*math.sin(math.radians(angle))+camera.y*math.cos(math.radians(angle)), camera.z), chars=brightness_sort())
+#     time.sleep(0.01)
+
+
+Separator.separate(polygons, 0.001)
